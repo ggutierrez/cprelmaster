febbb5d108a2e7a34374ccd2cd552586dd827005
Gustavo Gutierrez <gutierrez.gustavo@uclouvain.be>
2011-02-03 18:35:21 +0100

    Loading all the relations for guake

--- cdd-obj/cdd.hh
+++ cdd-obj/cdd.hh
@@ -5,7 +5,7 @@
 #include <list>
 #include <tr1/tuple>
 #include <exception>
-#include "cuddCDD.h" 
+#include "cuddCDD.h"
 
 // 32 bits integers max 8ary
 #define BBV 5
@@ -14,127 +14,126 @@
 //#define BBV 2
 //#define BA 1
 
-
 namespace Cdd {
   class failedMerge : public std::exception {
-		virtual const char* what(void) const throw() {
-			return "Failed operation on CDD";
-		}
-	};
+    virtual const char* what(void) const throw() {
+      return "Failed operation on CDD";
+    }
+  };
   class malformedDomain : public std::exception {
-		virtual const char* what(void) const throw() {
-			return "Malformed bounds in definition";
-		}    
+    virtual const char* what(void) const throw() {
+      return "Malformed bounds in definition";
+    }
   };
   /**
    * \brief BDD abstraction
    */
   class BDD {
-	private:
-		/// Function pointer
-		DdNode *node;
+  private:
+    /// Function pointer
+    DdNode *node;
     /// Create a BDD to representing \a p for a column \a a
     static BDD path(int p, int a);
   public:
     /// Constructor
     BDD(void);
-		/// Constructor from an existing node
-		BDD(DdNode *n);
-		/// Copy constructor
-		BDD(const BDD& from);
-		/// Returns the variable at possition \a v with a value of \a b
-		BDD(int v, bool b);
+    /// Constructor from an existing node
+    BDD(DdNode *n);
+    /// Copy constructor
+    BDD(const BDD& from);
+    /// Returns the variable at possition \a v with a value of \a b
+    BDD(int v, bool b);
     /// Constructs a BDD with the binary encoding of the elementes in [\a b, \a e]
     template <class I>
     BDD(I& b, I& e);
     /// Destructor
     ~BDD(void);
     static BDD create(std::tr1::tuple<int,int> t);
-		/// \name Test operations
-		//@{
-		/// BDD equality
-		bool operator == (const BDD& b) const;
-		/// BDD diesquality
-		bool operator != (const BDD& b) const;
-		//@}
-		/// \name Operations
-		//@{
-		/// BDD negation
-		BDD operator ! (void) const;
-		/// BDD negation
-		BDD operator ~ (void) const;
-		/// Assignment operation
-		BDD operator = (const BDD& right);
-		/// BDD intersection
-		BDD operator &= (const BDD& other);
-		/// BDD union
-		BDD operator |= (const BDD& other);
-		/// CDD merge
-		BDD operator << (const BDD& other) const;
-		/// CDD intersection
-		BDD operator && (const BDD& other) const;
-		/// CDD union
-		BDD operator || (const BDD& other) const;
+    /// \name Test operations
+    //@{
+    /// BDD equality
+    bool operator == (const BDD& b) const;
+    /// BDD diesquality
+    bool operator != (const BDD& b) const;
+    //@}
+    /// \name Operations
+    //@{
+    /// BDD negation
+    BDD operator ! (void) const;
+    /// BDD negation
+    BDD operator ~ (void) const;
+    /// Assignment operation
+    BDD operator = (const BDD& right);
+    /// BDD intersection
+    BDD operator &= (const BDD& other);
+    /// BDD union
+    BDD operator |= (const BDD& other);
+    /// CDD merge
+    BDD operator << (const BDD& other) const;
+    /// CDD intersection
+    BDD operator && (const BDD& other) const;
+    /// CDD union
+    BDD operator || (const BDD& other) const;
     /// CDD xor
     BDD operator ^ (const BDD& other) const;
     /// Status operation
     BDD status(const BDD& r) const;
     /// Delta operation
     BDD delta(void) const;
-		//@}
-		/// \name BDD information and statistics
-		//@{
-		/// Return the number of minterms of this BDD with a maximum number of variables of \a nvars
-		double minterms(int nvars) const;
-		/// Return the number of nodes in the BDD
-		int numNodes(void) const;
-		/// Print the tuples represented in the BDD for a relation with arity a
-		void printTuples(int a, std::ostream& os) const;
-		//@}
-		/// \name BDD output
-		//{@
-		/// Output the BDD in dot format to \a fname
-		void dot(const char* fname) const;
-		//@}
+    //@}
+    /// \name BDD information and statistics
+    //@{
+    /// Return the number of minterms of this BDD with a maximum number of variables of \a nvars
+    double minterms(int nvars) const;
+    /// Return the number of nodes in the BDD
+    int numNodes(void) const;
+    /// Print the tuples represented in the BDD for a relation with arity a
+    void printTuples(int a, std::ostream& os) const;
+    //@}
+    /// \name BDD output
+    //{@
+    /// Output the BDD in dot format to \a fname
+    void dot(const char* fname) const;
+    //@}
   };
-	  
-	class Cudd {
-		friend class BDD;
-	private:
-		static DdManager *dd;
-	public:
-		/// Constructor
-		Cudd(void);
-		/// Terminal one
-		static BDD one(void);
-		/// Terminal unknown
-		static BDD unk(void);
-		/// Terminal zero
-		static BDD zero(void);
+
+  class Cudd {
+    friend class BDD;
+  private:
+    static DdManager *dd;
+  public:
+    /// Constructor
+    Cudd(void);
+    /// Terminal one
+    static BDD one(void);
+    /// Terminal unknown
+    static BDD unk(void);
+    /// Terminal zero
+    static BDD zero(void);
 #ifndef NDEBUG
-		/// Return the number of referenced nodes in the manager
-		static int references(void);
+    /// Return the number of referenced nodes in the manager
+    static int references(void);
     /// Print statistics about CUDD
     static void stats(void);
 #endif
-	};
-	
-	class Relation {
+  };
+
+  class Relation {
   private:
     /// Domain representation
-		BDD r;
-		/// Arity
+    BDD r;
+    /// Arity
     int arity;
-		/**
+    /**
      * \brief Initialization of a constraint domain with \a lb and \a ub as the
      * lower and upper bound resp.
      *
      * \warning \a lb and \a ub are assumed to be ground relations
-     */    
-		void init(const BDD& lb, const BDD& ub);    
-	public:
-		/// Constructor for an empty relation of arity \a a
-		Relation(int a);
+     */
+    void init(const BDD& lb, const BDD& ub);
+  public:
+    /// Constructor for an empty relation of arity \a a
+    Relation(int a);
     /**
      * Constructor for the relation domain [\a lb, \a ub]
      *
@@ -146,14 +145,14 @@ namespace Cdd {
      */
     Relation(const BDD& dom, int a);
     /**
-     * \brief Initialization of a constraint domain with the relation represented 
+     * \brief Initialization of a constraint domain with the relation represented
      * by \a d.
      *
      * \warning \a d is assumed to be a domain representation: CDD or BDD
      */
     void init(const BDD& d);
-		/// Destructor
-		~Relation(void);
+    /// Destructor
+    ~Relation(void);
     /**
      * \brief Exclude the relation represented by \a g from the domain of this
      * variable.
@@ -162,13 +161,13 @@ namespace Cdd {
      * \warning An exception is triggered if the operation fails (e.g. tries to
      * remove elements already in the lower bound)
      */
-    void exclude(const BDD& g); 
+    void exclude(const BDD& g);
     Relation& operator >>= (const BDD& g);
-    
+
     void include(const BDD& g);
     Relation& operator <<= (const BDD& g);
-		/// Cardinality (number of tuples represented by the BDD \a b for a relation of this cardinality)
-		double cardinality(const BDD& b) const;
+    /// Cardinality (number of tuples represented by the BDD \a b for a relation of this cardinality)
+    double cardinality(const BDD& b) const;
     /// Return the lower bound of the relation (do not us this in propagation)
     const BDD glb(void) const;
     /// Return the upper bound of the relation (do not us this in propagation)
@@ -177,11 +176,11 @@ namespace Cdd {
     const BDD unk(void) const;
     /// Return the CDD representing the domain
     const BDD dom(void) const;
-	};
-  
+  };
+
   /// Output relation \a r to \a os
-  std::ostream& operator << (std::ostream& os, const Relation& r); 
-  
+  std::ostream& operator << (std::ostream& os, const Relation& r);
+
   template <class I>
   BDD::BDD(I& b, I& e) {
     node = Cudd::one();
--- tests/testcdd.cpp
+++ tests/testcdd.cpp
@@ -15,113 +15,115 @@ using namespace Cdd;
 void encode(unordered_map<int,int>& s, int v) {
   s[v] = s.size();
 }
-/** 
+/**
   Reads a file containing a binary relation. One pair per line.
  */
-void read_binrel(const char* fname) {
+BDD read_binrel(const char* fname) {
   unordered_map<int,int> normalize;
-  
+
+  BDD br = Cudd::zero();
+
   string line;
   ifstream input(fname);
   if (input.is_open()) {
-    BDD br = Cudd::zero();
     while (input.good()) {
       getline(input,line);
       stringstream ss(line);
       int first, second;
       ss >> first; ss >> second;
-      
+
       encode(normalize,first);
       encode(normalize, second);
       br |= BDD::create(make_tuple(first,second));
+      //tuples.push_back(make_pair(first,second));
     }
     input.close();
-    normalize.clear();
     cout << "Pairs in the relation: " << br.minterms(2) << endl;
-    Cudd::stats();
+    //Cudd::stats();
   } else {
     cerr << "Unable to open file: " << fname << endl;
   }
+  return br;
 }
 Relation xor_test(void) {
-  
+
   BDD lbr = Cudd::zero();
   BDD ubr = Cudd::zero();
-    
-  lbr |= 
+
+  lbr |=
     BDD::create(make_tuple(5,5)) ||
     BDD::create(make_tuple(5,6)) ||
     BDD::create(make_tuple(1,0));
 
-  ubr |= 
+  ubr |=
     BDD::create(make_tuple(5,5)) ||
     BDD::create(make_tuple(5,6)) ||
-    BDD::create(make_tuple(1,0)) || 
+    BDD::create(make_tuple(1,0)) ||
     BDD::create(make_tuple(2,3)) ||
     BDD::create(make_tuple(3,1)) ||
     BDD::create(make_tuple(1,1));
 
   Relation r(lbr, ubr, 2);
-  
-  
+
+
   BDD lbs = Cudd::zero();
   BDD ubs = Cudd::zero();
 
   lbs |=
     BDD::create(make_tuple(2,0)) ||
     BDD::create(make_tuple(3,1));
-  
+
   ubs |=
     BDD::create(make_tuple(2,0)) ||
     BDD::create(make_tuple(3,1)) ||
     BDD::create(make_tuple(5,5));
 
   Relation s(lbs, ubs, 2);
-  
+
   cout << s << endl;
   cout << "r" << endl;
   cout << r <<endl;
-  
+
   BDD doms = s.dom();
   BDD domr = r.dom();
-  
-//  BDD t = 
+
+//  BDD t =
   Relation x(doms ^ domr, 2);
   cout << "The lower bound" << endl;
   x.glb().printTuples(2,cout);
   cout << "the unkown" << endl;
   x.unk().printTuples(2,cout);
-  
+
 }
 
 Relation var_test(void) {
-  
-  
+
+
   BDD lb = Cudd::zero();
   BDD ub = Cudd::zero();
-  
+
   //{(1,2)}..{(1,2),(2,3),(0,0)}
   lb |= BDD::create(make_tuple(1,2));
   ub |= BDD::create(make_tuple(1,2));
   ub |= BDD::create(make_tuple(2,3));
   ub |= BDD::create(make_tuple(0,0));
-  
+
   // initialize r = [lb,ub]
   Relation r(lb, ub, 2);
-  
+
   //BDD glb = r.glb();
   //BDD dom = r.dom();
   //dom.dot("dom.dot");
   cout << r << endl;
-  
+
   // include glb in r
-  
+
   BDD g = Cudd::zero();
   g |= BDD::create(make_tuple(2,3));
-  
+
   //r <<= ub;
   //r >>= g;
-  
+
   cout << "after " << r << endl;
 
   return r;
@@ -129,52 +131,47 @@ Relation var_test(void) {
 
 void rel_test(void) {
   Relation r = xor_test();
-  
+
 }
 
 BDD delta_test() {
   BDD zero = Cudd::zero();
   BDD unk = Cudd::unk();
   BDD one = Cudd::one();
-  
+
   BDD v0(0,true);
   BDD v1(1,true);
   BDD v2(2,true);
   BDD v3(3,true);
-  
+
   BDD t = unk;
   t &= v0; t &= v1; t &= v2; t &= v3;
-  
+
   //t &= unk;
-  BDD u = t.delta();  
+  BDD u = t.delta();
   t.dot("input.dot");
   u.dot("out.dot");
-  //BDD v = unk.delta();  
-  
+  //BDD v = unk.delta();
+
   //cout << "Expected ret one " << (u == unk) << endl;
   //cout << "Expected ret one " << (u == one) << endl;
   //cout << "Expected ret unk " << (v == unk) << endl;
-  
-  
+
+
   return t;
 }
 
 int main(void) {
   cout << "Start of the test" << endl;
-  read_binrel("deps-guake.txt");
-  /*
-   Relation r = var_test();
-   BDD glb = r.glb();
-   BDD unk = r.unk();
-   */
-  //BDD r = delta_test();
-  //rel_test();
-  //unk.printTuples(2);
-  
-  //cout << "Minterms in glb " << lb.minterms(2) << " Nodes: " << lb.numNodes() << endl;
-  //cout << "Minterms in lub " << ub.minterms(2) << " Nodes: " << ub.numNodes() << endl;		
-  //cout << "test " << (r.path(2) == one) << endl;
-	
-  cout << "This number should be zero: " << Cudd::references() << endl;
+  BDD deps = read_binrel("deps-guake.txt");
+  BDD confs = read_binrel("conf-guake.txt");
+  BDD provs = read_binrel("prov-guake.txt");
+
+  Relation Dep(deps,Cudd::one(),2);
+  Relation Conf(confs,Cudd::one(),2);
+  Relation Prov(Cudd::zero(),provs,2);
+
+  //cout << "elements in all " << all.size() << endl;
+  //cout << "This number should be zero: " << Cudd::references() << endl;
   return 0;
 }
