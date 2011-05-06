fe3dde3bdeb847bdf0efd6e0945c3813c0ceda64
gg <gg1@gg-zzz666664.(none)>
2011-05-06 19:42:57 +0200

    A domain library containing the bdd implementation of the domain is provided.
    Iterator on a domain is implemented and memory leaks were fixed. (clean
    valgrind)

--- tests/domain-test.cc
+++ tests/domain-test.cc
@@ -2,6 +2,20 @@
 #include <vector>
 #include <bdddomain/relation.hh>
 
+using std::ostream;
+using std::vector;
+
+ostream& operator<<(ostream& os, const vector<int>& tuple) {
+  os << "<";
+  for (unsigned int i = 0; i < tuple.size(); i++) {
+    os << tuple[i];
+    if (i != tuple.size()-1)
+      os << ",";
+  }
+  os << ">";
+  return os;
+}
+
 int main(void) {
   using namespace std;
   using namespace BDDImpl;
@@ -11,20 +25,26 @@ int main(void) {
     data.push_back(make_pair(0,1));
     data.push_back(make_pair(1,0));
     data.push_back(make_pair(1,1));
-    
+
     GRelation rr(2,data);
+    rr.print(cerr);
+
     BddIter it(rr.tuples());
+    for (; it(); )
+      cout << it.val() << endl;
+    /*
     BddIter it2(rr.tuples());
 
-    it.val();
+
     it.val();
     it.val();
     it.val();
     cout << "Valid? " << it() << endl;
     it2.val();
     cout << "Valid? " << it2() << endl;
+    */
   }
   cout << "References before exit: " << BDDConfig::references() << endl;
-  //Cudd_Quit(BDDConfig::dd);
+
   return 0;
 }
