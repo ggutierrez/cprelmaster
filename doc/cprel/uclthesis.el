b15716f038426ebcbefc9391d975e5c1bb75cae8
gg <gg@ggs-iMac.local>
2011-09-07 23:31:06 +0200

    Added FAQ

--- doc/cprel/uclthesis.el
+++ doc/cprel/uclthesis.el
@@ -9,3 +9,19 @@
  	       ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
  	       ("\\paragraph{%s}" . "\\paragraph*{%s}")
  	       ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+(setq org-export-latex-listings 'minted)
+(setq org-export-latex-custom-lang-environments
+      '(
+	(emacs-lisp "common-lispcode")
+	))
+(setq org-export-latex-minted-options
+      '(
+	("bgcolor" "codebg")
+	("frame" "single")
+	("fontsize" "\\scriptsize")
+	("linenos" "")))
+(setq org-latex-to-pdf-process
+      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+	"pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+	"pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
