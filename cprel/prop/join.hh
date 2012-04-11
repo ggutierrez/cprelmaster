fd9b73af9818975b61637369ac25dc0f4b4bdbe6
Gustavo Gutiérrez <gustavo.ggutierrez@gmail.com>
2012-04-11 09:34:00 +0200

    Fixed propagation rules or join constraint according with yesterday's meeting

--- cprel/prop/join.hh
+++ cprel/prop/join.hh
@@ -78,34 +78,26 @@ public:
     // C must have atleast the join of what is known in A and B
     GRelation glbJ = a_.glb().join(j_,b_.glb());
     GECODE_ME_CHECK(c_.include(home,glbJ));
-
+    
     // C can have atmost the join of what is possible in A and B
     GRelation max_possile_left = a_.lub().join(j_,b_.lub());
     GECODE_ME_CHECK(c_.exclude(home,max_possile_left.complement()));
-
-    /*GRelation needed_by_blub = a_.glb().join(j_,b_.lub());
+    
+    GRelation needed_by_blub = a_.glb().join(j_,b_.lub());
     GRelation no_support_c_blub =
-        needed_by_blub.difference(c_.lub()).project(b_.arity());
+      needed_by_blub.difference(c_.lub()).project(b_.arity());
     GECODE_ME_CHECK(b_.exclude(home,no_support_c_blub));
-
+    
     GRelation needed_by_alub = a_.lub().join(j_,b_.glb());
     GRelation no_support_c_alub =
-        needed_by_alub.difference(c_.lub()).shiftRight(b_.arity()-j_);
-    GECODE_ME_CHECK(a_.exclude(home,no_support_c_alub));*/
-
-    /*GRelation bc_lub = compute_bc_lub();
-    GRelation y = b_.lub().intersect(bc_lub);
-    GECODE_ME_CHECK(b_.exclude(home,y.complement()));*/
-
+      needed_by_alub.difference(c_.lub()).shiftRight(b_.arity()-j_);
+    GECODE_ME_CHECK(a_.exclude(home,no_support_c_alub));
+    
     GRelation bc_glb = compute_bc_glb();
     GECODE_ME_CHECK(b_.include(home,bc_glb));
-
+    
     GRelation ac_glb = compute_ac_glb();
     GECODE_ME_CHECK(a_.include(home,ac_glb));
-
-    /*GRelation ac_lub = compute_ac_lub();
-    GRelation z = a_.lub().intersect(ac_lub);
-    GECODE_ME_CHECK(a_.exclude(home,z.complement()));*/
 
     // Propagator subsumption
     if (a_.assigned() && b_.assigned() && c_.assigned()) {
