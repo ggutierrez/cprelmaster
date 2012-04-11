e964b9cfd87d8b9128f41ba2f8f56a334308edee
Gustavo Gutiérrez <gustavo.ggutierrez@gmail.com>
2012-04-11 17:44:50 +0200

    Checked follow propagator rules

--- cprel/prop/follow.hh
+++ cprel/prop/follow.hh
@@ -145,6 +145,7 @@ public:
       GRelation max_possile_c = a_.lub().follow(f_,b_.lub());
       GECODE_ME_CHECK(c_.exclude(home,max_possile_c.complement()));
     }
+
     // 2) pruning A from B and C
     {
       // pruning the lower bound of A
@@ -155,8 +156,8 @@ public:
       GECODE_ME_CHECK(a_.include(home,singles));
 
       // pruning the upper bound of A
-      /*GRelation y = c_.oob().follow(b_.arity() - f_, TransformB(b_.glb()));
-      GECODE_ME_CHECK(a_.exclude(home,y));*/
+      GRelation y = c_.oob().follow(b_.arity() - f_, TransformB(b_.glb()));
+      GECODE_ME_CHECK(a_.exclude(home,y));
     }
 
     // 3) Pruning B from A and C
@@ -169,9 +170,9 @@ public:
       GECODE_ME_CHECK(b_.include(home,singles));
 
       // pruning the upper bound of B
-      /*GRelation y = transformC(c_.oob()).follow(a_.arity() - f_, a_.glb());
+      GRelation y = transformC(c_.oob()).follow(a_.arity() - f_, a_.glb());
       GRelation b_out = TransformB(y);
-      GECODE_ME_CHECK(b_.exclude(home,b_out));*/
+      GECODE_ME_CHECK(b_.exclude(home,b_out));
     }
 
     // Propagator subsumption
