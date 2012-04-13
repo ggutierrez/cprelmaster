71cc22e5ec61b956bd9a245453324b06a437c3e7
Gustavo Gutiérrez <gustavo.ggutierrez@gmail.com>
2012-04-13 20:27:31 +0200

    forgot the test file in the last commit

--- tests/gcp.cpp
+++ tests/gcp.cpp
@@ -8,8 +8,12 @@ class GCP : public Gecode::Space {
 protected:
   GRelation graph;
   GRelation colors;
-  CPRelVar colorLeft, colorRight, colorMapping;
-  CPRelVar tmp;
+  //CPRelVar possibles, colorCT, graphCT;
+  CPRelVar r, r_prime;
+  CPRelVar tmp, tmp2;
+  CPRelVar tmp3;
+  //, tmp2, G;
+  CPRelVar G; // a constraint variable with the graph, fixed
 public:
   GCP(void) : graph(2), colors(1) 
   {
@@ -17,9 +21,12 @@ public:
     {
       graph.add(make_Tuple(1,2));
       graph.add(make_Tuple(2,3));
-      graph.add(make_Tuple(3,4));
+      //graph.add(make_Tuple(3,4));
     }
-    CPRelVar G(*this,graph,graph);
+    G = CPRelVar(*this,graph,graph);
+    //tmp = CPRelVar(*this,GRelation(2),graph);
+    //subset(*this,G,tmp);
+ 
     // colors
     {
       colors.add(make_Tuple(10));
@@ -30,38 +37,70 @@ public:
       colorEq.add(make_Tuple(10,10));
       colorEq.add(make_Tuple(11,11));
     }
-    CPRelVar colorsForb(*this,colorEq,colorEq);
+    GRelation possibleColors = colors.times(colors).difference(colorEq);
+    tmp3 = CPRelVar(*this,possibleColors,possibleColors);
     
-    // initialize the color mapping to contain every possible combination of colors.
-    colorLeft = CPRelVar(*this,GRelation(4),colors.times(graph.times(colors)));
+    GRelation r_max = graph.times(possibleColors);
+    //cout << "R max" << endl << r_max << endl;
+    r = CPRelVar(*this,GRelation(4),r_max);
 
-    std::vector<std::pair<int,int> >p;
-    p.push_back({3,1});
-    colorRight = CPRelVar(*this,GRelation(4),GRelation::create_full(4));
-    permutation(*this,colorLeft,colorRight,p);
+    r_prime = CPRelVar(*this,GRelation(4),GRelation::create_full(4));
+    std::vector<std::pair<int,int> > perm = {{1,3},{2,0}};
+    permutation(*this,r,r_prime,perm);
     
-    tmp = CPRelVar(*this,GRelation(2),GRelation::create_full(2));
-    projection(*this,2,colorRight,tmp);
+    tmp = CPRelVar(*this,GRelation(2),graph);
+    projection(*this,2,r_prime,tmp);
+    subset(*this,G,tmp);
 
-    //subset(*this,G,colorLeft_{first two});
-    disjoint(*this,tmp,colorsForb);
-    branch(*this,colorLeft);
+    tmp2 = CPRelVar(*this,GRelation(2),possibleColors);
+    projection(*this,2,r,tmp2);
+    subset(*this,tmp2,tmp3);
+         
+    branch(*this,r_prime);
 }
   void print(std::ostream& os) const {
     os << std::endl << std::endl 
       //<< "ColorMapping: " << std::endl
       // << tmp << std::endl
-       << "ColorLeft:" << std::endl
-       << colorLeft << std::endl
-      //<< "ColorRight:" << std::endl
-      // << colorRight 
+      // << "Possibles:" << std::endl
+      // << possibles << std::endl
+      // << "ColorCT:" << std::endl
+      // << colorCT
+      // << "GraphCT:" << std::endl
+      // << graphCT
+       << "R:" << std::endl
+       << r
+       << "Tmp: " << std::endl
+       << tmp
+       << endl
+       << "Tmp2: " << std::endl
+       << tmp2
+       << endl
+       << "R':" << std::endl
+       << r_prime
+      
+      //<< "Tmp3: " << tmp3 << endl
+      
+      //<< "G:" << endl
+      // << G
        << std::endl;
   }
   GCP(bool share, GCP& s)
     : Gecode::Space(share,s), graph(s.graph), colors(s.colors) {
-    tmp.update(*this,share,s.tmp);
-    colorLeft.update(*this,share,s.colorLeft);
-    colorRight.update(*this,share,s.colorRight);
+    G.update(*this,share,s.G);
+    r.update(*this,share,s.r);
+    r_prime.update(*this,share,s.r_prime);
+    tmp.update(*this,share,s.tmp);    
+    tmp2.update(*this,share,s.tmp2);    
+    tmp3.update(*this,share,s.tmp3);    
+    /*
+
+    tmp2.update(*this,share,s.tmp2);
+    
+    possibles.update(*this,share,s.possibles);
+    colorCT.update(*this,share,s.colorCT);
+    graphCT.update(*this,share,s.graphCT);
+    */
   }
   virtual Space* copy(bool share) {
     return new GCP(share,*this);
@@ -72,13 +111,16 @@ public:
 int main(int, char**) {
   GCP* g = new GCP;
 
+  int sols = 0;
   Gecode::DFS<GCP> e(g);
   delete g;
   std::cout << "Search will start" << std::endl;
   while (Gecode::Space* s = e.next()) {
+    std::cout << "Next solution: " << std::endl;
     static_cast<GCP*>(s)->print(std::cout);
     delete s;
+    sols++;
   }
-  std::cout << "Finishing" << std::endl;
+  std::cout << "Finishing: " << sols << std::endl;
   return 0;
 }
--- tests/join3.cpp
+++ tests/join3.cpp
@@ -1,11 +1,43 @@
 #include <gecode/search.hh>
 #include <cprel/cprel.hh>
+#include <vector>
+#include <algorithm>
 
 using namespace Gecode;
 using std::pair;
 using std::make_pair;
 using namespace MPG;
 using namespace MPG::CPRel;
+
+using std::vector;
+
+template <typename Ta, typename Tb>
+void mySwap(Ta& a, Tb& b, int posa, int posb) {
+  cout << "swapping " << a << " -- " << b << endl;
+  std::swap(a,b);
+}
+
+void REORDER(vector<char>& vA, vector<size_t>& vOrder)  
+{   
+  assert(vA.size() == vOrder.size());
+
+  // for all elements to put in place
+  for( int i = 0; i < vA.size() - 1; ++i ) { 
+    // while the element i is not yet in place 
+    while( i != vOrder[i] )
+      {
+        // swap it with the element at its final place
+        int alt = vOrder[i];
+        /*
+          std::swap( vA[i], vA[alt] );
+          std::swap( vOrder[i], vOrder[alt] );
+        */
+        mySwap( vA[i], vA[alt], i, alt );
+        mySwap( vOrder[i], vOrder[alt], i, alt );
+      }
+  }
+}
+
 
 std::vector<std::pair<int,int>> permJoin(int n, int m, int j, int k) {
   // | n-j | j | m-j |
@@ -70,7 +102,7 @@ pair<GRelation,GRelation> domT(void) {
 class JoinTest : public Gecode::Space {
 protected:
   CPRelVar r,s,t;
-  CPRelVar joinResult, followAllResult;
+  CPRelVar joinResult, followAllResult, permT;
 public:
   JoinTest(void)  {
 
@@ -96,24 +128,28 @@ public:
     followAllResult = CPRelVar(*this,GRelation(arityFollow),GRelation::create_full(arityFollow));
     followAll(*this,r,j,s,followAllResult);
 
-    CPRelVar u(*this,GRelation(j),GRelation::create_full(j));
+    CPRelVar u(*this,GRelation::create_full(j),GRelation::create_full(j));
     CPRelVar times(*this,GRelation(arityJoin),GRelation::create_full(arityJoin));
     join(*this,followAllResult,0,u,times);
 
     CPRelVar permJoinResult(*this,GRelation(arityJoin),GRelation::create_full(arityJoin));
     permutation(*this,joinResult,permJoinResult,permJoin(n,m,j,k));
 
-    CPRelVar permT(*this,GRelation(k),GRelation::create_full(k));
-    permutation(*this,t,permT,permC(n,m,j,k));
-    
-    branch(*this,t);
+    permT = CPRelVar(*this,GRelation(k),GRelation::create_full(k));
+    //permutation(*this,t,permT,permC(n,m,j,k));
+    //permutation(*this,t,permT,{{3,0},{3,2},{0,1},{2,1},{1,2},{1,0}});
+    permutation(*this,t,permT,{{3,0},{3,2},{0,1},{2,1},{1,2},{1,0}});
+    //branch(*this,t);
     //restrJoinAll(*this,r,j,s,t);
   }
   void print(std::ostream& os) const {
     os << "Print method called" << std::endl;
-    os << "R: " << r << endl;
-    os << "S: " << s << endl;
+    //os << "R: " << r << endl;
+    //os << "S: " << s << endl;
     os << "T: " << t << endl;
+
+    os << "PermT: " << permT << endl;
+
 
     //os << "JoinResult: " << joinResult << endl;
     //os << "followAllResult: " << followAllResult << endl;
@@ -125,7 +161,9 @@ public:
     r.update(*this, share, sp.r);
     t.update(*this, share, sp.t);
     s.update(*this, share, sp.s);
-    
+
+    permT.update(*this, share, sp.permT);
+
     joinResult.update(*this, share, sp.joinResult);
     followAllResult.update(*this, share, sp.followAllResult);
   }
@@ -135,6 +173,19 @@ public:
 };
 
 int main(int, char**) {
+  /*
+  char   A[]     = { '3', '2', '1', '0' };
+  size_t ORDER[] = { 3, 0, 1, 2 };
+
+  vector<char>   vA(A, A + sizeof(A) / sizeof(*A));
+  vector<size_t> vOrder(ORDER, ORDER + sizeof(ORDER) / sizeof(*ORDER));
+
+  REORDER(vA, vOrder);
+
+  for (auto& a : vA)
+    cout << a << endl;
+  */
+  
   JoinTest* g = new JoinTest();
   DFS<JoinTest> e(g);
 
@@ -144,5 +195,6 @@ int main(int, char**) {
     delete s;
   }
   delete g;
+  
   return 0;
 }
