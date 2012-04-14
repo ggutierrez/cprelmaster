17cee59b118562de2766ad4d4ec250c926805587
svancauw <sascha.vancauwelaert@gmail.com>
2012-04-14 12:38:41 +0200

    join3.cpp correct (contains a problem that impose the joinAll constraint rules)

--- tests/join3.cpp
+++ tests/join3.cpp
@@ -7,24 +7,116 @@ using std::make_pair;
 using namespace MPG;
 using namespace MPG::CPRel;
 
-std::vector<std::pair<int,int>> permJoin(int n, int m, int j, int k) {
+
+//put the joined part on the right
+std::vector<std::pair<int,int>> putJoinedPartOnRight(int j, int m) 
+{
+  
+  std::vector<std::pair<int,int>> result;
+  if (j >= m - j)
+	{
+		for (int i = 0; i < m - j  ; i++)
+	    	result.push_back(std::make_pair(i,i + j));
+	  
+	}
+	else //if(j < m - j )
+	{
+		cout << "m -j " << m - j << endl;
+		cout << "m : " << m << " and j : " << j << endl;
+		for (int i = 0; i <= j - 1 ; i++)
+	    	result.push_back(std::make_pair(i,i + m - j ));
+	}
+  
+  return result;
+}
+
+//create the permutation description needed to reorder one columns if j!= m - j
+//indexStart is the number of the column that will be reorder
+//indexEnd is the number of the column to which the column indexStart will go
+std::vector<std::pair<int,int>> permJoinReorderOne(int indexStart, int indexEnd) 
+{
+	
+  std::vector<std::pair<int,int>> result;
+
+cout << "reorder one" << endl;
+ 
+cout << "indexStart : " << indexStart << "indexStart + indexEnd - 1 : " <<  indexEnd - 1 << endl;
+
+  for (int i = indexStart; i < indexEnd ; i++)
+    result.push_back(std::make_pair(i,i+1));
+  
+  return result;
+}
+
+
+//create the permutation description needed to reorder columns if j!= m - j
+std::vector<std::pair<int,int>> permJoinReorder(int j, int m) 
+{
+
+  std::vector<std::pair<int,int>> result;
+  std::vector<std::pair<int,int>> temp;  
+
+  //if the joined part is smaller
+  if(j < m - j)
+	{
+		for(int i = m - j - 1 ; i >= j ; i--)
+		{
+			cout << "i " << i << endl; 
+			cout << "i + j " << i + j << endl; 
+			temp = permJoinReorderOne(i,i+j);
+			copy (temp.begin(),temp.end(),back_inserter(result));
+		}
+	}
+	//if the joined part is bigger
+	else if(j > m - j)
+	{
+		cout << "j > m - j" << endl ;
+		for(int i = m - j - 1 ; i >= 0 ; i--)
+		{
+			cout << " m - j - 1 : " << m - j - 1 << endl ;
+			cout << "permJoinReorderOne(i,i + j - 1); with : i : " << i << "and i + j - 1 : "  << i + j - 1 << endl ;
+			temp = permJoinReorderOne(i,i + j - 1);
+			copy (temp.begin(),temp.end(),back_inserter(result));
+		}
+		
+	}
+
+  return result;
+}
+
+
+std::vector<std::pair<int,int>> permJoin(int m, int j) {
   // | n-j | j | m-j |
   // ->
   // | n-j | m-j | j |
-  std::vector<std::pair<int,int>> result;
+  std::vector<std::pair<int,int>> result; 
+  std::vector<std::pair<int,int>> temp;
   
-  for (int i = 0; i < m-j; i++)
-    result.push_back(std::make_pair(i,i+j));
-  int s = 0;
-  for (int i = m-j; i < m; i++)  {
-    result.push_back(std::make_pair(i,s));
-    s++;
-  }
+  //first we put the joined column on the right
+  result = putJoinedPartOnRight(j,m);
+
+cout << "before reorder part " << endl ;
+
+for(auto i = result.begin() ; i!=result.end() ; ++i)
+	std::cout << i->first << "|" << i->second << std::endl;
+
+  //now we adapat if needed
+  if (j!=m)
+	{
+		temp = permJoinReorder(j,m);
+		copy (temp.begin(),temp.end(),back_inserter(result));
+	}
+	
+	cout << " after reorder part" << endl;
+	
+	for(auto i = result.begin() ; i!=result.end() ; ++i)
+		std::cout << i->first << "|" << i->second << std::endl;
+		
   return result;
 }
   
 // Find the permutation for the result of the join
-std::vector<std::pair<int,int>> permC(int n, int m, int j, int k) {
+/*std::vector<std::pair<int,int>> permC(int n, int m, int j, int k) {
   // | n-j | m-j | j |
   // ->
   // | n-j | j | m-j |
@@ -38,7 +130,7 @@ std::vector<std::pair<int,int>> permC(int n, int m, int j, int k) {
     s++;
   }
   return result;
-}
+}*/
 
 
 pair<GRelation,GRelation> domR(void) {
@@ -70,7 +162,7 @@ pair<GRelation,GRelation> domT(void) {
 class JoinTest : public Gecode::Space {
 protected:
   CPRelVar r,s,t;
-  CPRelVar joinResult, followAllResult;
+  CPRelVar joinResult, followAllResult, permT, permJoinResult, times;
 public:
   JoinTest(void)  {
 
@@ -96,24 +188,32 @@ public:
     followAllResult = CPRelVar(*this,GRelation(arityFollow),GRelation::create_full(arityFollow));
     followAll(*this,r,j,s,followAllResult);
 
-    CPRelVar u(*this,GRelation(j),GRelation::create_full(j));
-    CPRelVar times(*this,GRelation(arityJoin),GRelation::create_full(arityJoin));
+    CPRelVar u(*this,GRelation::create_full(j),GRelation::create_full(j));
+    times = CPRelVar(*this,GRelation(arityJoin),GRelation::create_full(arityJoin));
     join(*this,followAllResult,0,u,times);
 
-    CPRelVar permJoinResult(*this,GRelation(arityJoin),GRelation::create_full(arityJoin));
-    permutation(*this,joinResult,permJoinResult,permJoin(n,m,j,k));
+    permJoinResult = CPRelVar(*this,GRelation(arityJoin),GRelation::create_full(arityJoin));
+    permutation(*this,joinResult,permJoinResult,permJoin(m,j));
 
-    CPRelVar permT(*this,GRelation(k),GRelation::create_full(k));
-    permutation(*this,t,permT,permC(n,m,j,k));
+    permT = CPRelVar(*this,GRelation(k),GRelation::create_full(k));
+    permutation(*this,t,permT,permJoin(m,j));
     
-    branch(*this,t);
+	intersect(*this,times,permJoinResult,permT);
+    //branch(*this,t);
     //restrJoinAll(*this,r,j,s,t);
   }
   void print(std::ostream& os) const {
+	
     os << "Print method called" << std::endl;
     os << "R: " << r << endl;
     os << "S: " << s << endl;
+    os << "joinResult: " << joinResult << endl;
+	os << "permJoinResult: " << permJoinResult << endl;
+	os << "followAllResult: " << followAllResult.assigned() << endl;
+	os << "times: " << times.assigned() << endl;
     os << "T: " << t << endl;
+	os << "permT: " << permT << endl;
+	
 
     //os << "JoinResult: " << joinResult << endl;
     //os << "followAllResult: " << followAllResult << endl;
@@ -125,9 +225,12 @@ public:
     r.update(*this, share, sp.r);
     t.update(*this, share, sp.t);
     s.update(*this, share, sp.s);
+	permT.update(*this, share, sp.permT);
+	permJoinResult.update(*this, share, sp.permJoinResult);
+	joinResult.update(*this, share, sp.joinResult);
+	followAllResult.update(*this, share, sp.followAllResult);
+	times.update(*this, share, sp.times);
     
-    joinResult.update(*this, share, sp.joinResult);
-    followAllResult.update(*this, share, sp.followAllResult);
   }
   virtual Space* copy(bool share) {
     return new JoinTest(share,*this);
